3bd7d1610eefe15f55389191bab96de08f86ded7
bilgitap <bilgitap@hotmail.com>
2020-04-27 04:22:36 +0300

    changing the backend panel template

--- resources/views/backend/blank.blade.php
+++ resources/views/backend/blank.blade.php
@@ -1,45 +1,24 @@
-@extends('backend/layouts/default')
+@extends('backend/layouts/main')
 
-{{-- Page title --}}
-@section('title')
-Blank Page
-@parent
-@stop
-
-{{-- page level styles --}}
-@section('header_styles')
-<link rel="stylesheet" href="{{ asset('backend/css/pages/news.css') }}" />
-<style>
-    body {
-        overflow: -webkit-paged-x;
-    }
-</style>
-@stop
-
-{{-- Page content --}}
 @section('content')
-
-<section class="content-header">
-    <h1>Blank page</h1>
-    <ol class="breadcrumb">
-        <li>
-            {{-- <a href="{{ route('admin.dashboard') }}"> --}}
-            <a>
-                <i class="livicon" data-name="home" data-size="14" data-color="#000"></i>
-                Dashboard
-            </a>
-        </li>
-        <li><a href="#"> Pages</a></li>
-        <li class="active">Blank page</li>
+<div class="page-header mt-0 shadow p-3">
+    <ol class="breadcrumb mb-sm-0">
+        <li class="breadcrumb-item"><a href="#">Pages</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Empty Page</li>
     </ol>
-</section>
-<section class="content">
-</section>
-
-
-@stop
-
-{{-- page level scripts --}}
-@section('footer_scripts')
-
-@stop
+    <div class="btn-group mb-0">
+        <button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+            aria-expanded="false">Actions</button>
+        <div class="dropdown-menu">
+            <a class="dropdown-item" href="#"><i class="fas fa-plus mr-2"></i>Add new
+                Page</a>
+            <a class="dropdown-item" href="#"><i class="fas fa-eye mr-2"></i>View the page
+                Details</a>
+            <a class="dropdown-item" href="#"><i class="fas fa-edit mr-2"></i>Edit Page</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i> Settings</a>
+        </div>
+    </div>
+</div>
+<div style="height:80vh"></div>
+@endsection
